403607ef772998c412365384b60f8b24f6c24406
Timor <timorbystritskie@gmail.com>
2022-11-21 07:53:48 +0200

    - Created the params file - Created initial implementation of store manager

---
+++ Assets/Scripts/MarketParameters.cs
@@ -0,0 +1,32 @@
+/*This module contatins all of the simualtor params*/
+
+/*Customer Params*/
+public class CustomerParams
+{
+    public const int CUSTOMER_COUNT = 5;
+    public const int TTL = 3;
+    public const float ALPHA;
+}
+
+
+
+/*Store Params*/
+public class StoreParams
+{
+    public const int SHOP_COUNT = 10;
+    public const int SHOP_STARTING_BALANCE = 250;
+    public const int BASE_TAX = 150;
+    public const float ALPHA;
+    public const float BETA;
+    public const float GAMA;
+    public const float INVESMENT_TENDENCY;
+
+}
+
+
+/*Market Params*/
+public class MarketParams
+{
+    VISUAL_TRADING_MODE = true;
+}
+
---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShopManger: MonoBehavior
+{
+	[SerializeField] GameObject sfolder; // Store folder
+	[SerializeField] GameObject ground;
+	[SerializeField] int MAX_STORE;
+	static int currentStoreCount;
+	static int balance = BASE_BALANCE;
+
+	void start()
+    {
+		//TO DO 
+    }
+
+	void update()
+    {
+		//TO DO 
+    }
+
+	private void spawn()
+    {
+		// TO DO
+    }
+
+	public Vector3 GetRandomPostionInBounds()
+    {
+		//TO DO
+    }
+}
