1f17b0ae484b1a1842280687213405b3b9989b9f
Timor <timorbystritskie@gmail.com>
2022-11-27 14:14:07 +0200

    temporary

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -3,31 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ShopManger: MonoBehavior
+public class StoreManager: MonoBehaviour
 {
 	[SerializeField] GameObject sfolder; // Store folder
 	[SerializeField] GameObject ground;
-	[SerializeField] int MAX_STORE;
-	static int currentStoreCount;
-	static int balance = BASE_BALANCE;
+	[SerializeField] GameObject stores;
+	[SerializeField] int MAX_STORES = 10;
+	static int currentStoreCount = 0;
+	static int _MAX_STORES;
+	static int balance;// = BASE_BALANCE;
+	static object _lock = new object();
 
-	void start()
+	private Bounds storeBounds;
+	private Bounds bounds;
+	private bool bankcrupt;
+
+	void Start()
     {
-		//TO DO 
+		_MAX_STORES = MAX_STORES;
+        bounds = ground.GetComponent<Renderer>().bounds;
+		storeBounds = stores.GetComponent<Renderer>().bounds;
     }
 
-	void update()
+	void Update()
     {
-		//TO DO 
+		if (currentStoreCount < MAX_STORES)
+			Spawn();
     }
 
-	private void spawn()
+    public static void SusspendsMe(StoreController sc)
+	{
+		lock(_lock)
+		{
+			currentStoreCount--;
+			// need to think how we actually suspend the store from business
+		}
+
+	}
+
+	private void Spawn()
     {
-		// TO DO
+		for (int i=0; currentStoreCount < MAX_STORES; i++) 
+		{
+			GameObject store = Instantiate<GameObject>(Resources.Load<GameObject>("Store"));
+			store.transform.position = GetRandomPostionInBounds();
+			store.transform.SetParent(sfolder.transform);
+			
+			currentStoreCount++;
+		}
     }
 
 	public Vector3 GetRandomPostionInBounds()
     {
-		//TO DO
+		return new Vector3(
+			Random.Range(bounds.min.x - storeBounds.min.x, bounds.max.x - storeBounds.max.x),
+			bounds.max.y,
+			Random.Range(bounds.min.z - storeBounds.min.z, bounds.max.z - storeBounds.max.z)
+			);
     }
 }
